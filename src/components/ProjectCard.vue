92676c213cc1bfcffc8671425a934b1216b3d1b7
Angelop26 <angeloprospero26@gmail.com>
2023-06-14 18:54:18 +0200

    bonus 2 completed

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -12,6 +12,24 @@ export default {
         <div class="card-title">
             <h6>{{ project.title }}</h6>
         </div>
+        <div class="card-body">
+
+            <h6>lista tecnologie</h6>
+            <ul v-if="(project.technologies.length > 0)">
+                <li   v-for="elem,index in project.technologies" :key="index">{{ elem.name }}</li>
+            </ul>
+            <div v-else>
+                <p> non ci sono tecnologie </p>
+            </div>
+
+            <h6>lista tipi</h6>
+            <ul v-if="(project.type)">
+                <li v-for="elem,index in project.type" :key="index">{{ elem.name }}</li>
+            </ul>
+            <div v-else>
+                <p> non ci sono tipi </p>
+            </div>
+        </div>
     </div>
 </template>
 
