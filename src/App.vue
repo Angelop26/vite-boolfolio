a1ce3e3958904b90b1f1b247aef9d1795da07b58
Angelop26 <angeloprospero26@gmail.com>
2023-06-14 18:30:05 +0200

    bonus 1 completed

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,9 @@ export default{
   data(){
     return{
       dataUrl:"http://127.0.0.1:8000",
-      projects:[]
+      projects:[],
+      current_page:'',
+      last_page:''
     };
   },
 
@@ -17,9 +19,14 @@ export default{
   },
 
   methods: {
-    getProjects(){
-      axios.get(`${this.dataUrl}/api/homeapi`).then(resp => {
-        this.projects = resp.data.results;
+    getProjects(page = 1){
+      const params = {
+        page:page
+      }
+      axios.get(`${this.dataUrl}/api/homeapi`, {params}).then(resp => {
+        this.projects = resp.data.results.data;
+        this.current_page = resp.data.results.current_page;
+        this.last_page = resp.data.results.last_page
       })
     }
   }
@@ -34,6 +41,14 @@ export default{
       <ProjectCard :project ="project" />
     </div>
   </div>
+
+  <nav aria-label="Page navigation example ">
+  <ul class="pagination justify-content-center mt-3">
+    <li class="page-item"><a class="page-link" :class="{'disabled': current_page === 1}"  @click.prevent="getProjects(current_page - 1)" href="">Previous</a></li>
+    <li class="page-item" v-for="(pageNum,index) in last_page" :key="index"><a class="page-link" :class="{'active': current_page === pageNum}"  @click.prevent="getProjects(pageNum)" href="">{{pageNum}}</a></li>
+    <li class="page-item"><a class="page-link" :class="{'disabled': current_page === last_page}"  @click.prevent="getProjects(current_page + 1)" href="">Next</a></li>
+  </ul>
+</nav>
 </div>
 </template>
 
